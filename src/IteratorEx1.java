352fa9d62504b5166f93ec0e12aefc62494658a4
mdziuba <maciejdziuba92@gmail.com>
2022-10-07 12:58:54 +0200

    small change in code, deleted bracers that crashed compilation

--- src/IteratorEx1.java
+++ src/IteratorEx1.java
@@ -20,7 +20,7 @@ public class IteratorEx1 {
         Iterator<Person> iterator = people.iterator();
 
         while (iterator.hasNext()){
-            System.out.println(iterator.next());()
+            System.out.println(iterator.next());
         }
 
     }
